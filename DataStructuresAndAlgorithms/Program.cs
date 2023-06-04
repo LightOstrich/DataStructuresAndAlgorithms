0ed99487a45509f0405ad29e47d776bffbff6425
Abdullo <abdullakhzukhriddinov@gmail.com>
2023-06-04 22:29:19 +0500

    Adding new data structure

--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -4,7 +4,18 @@
     {
         public static void Main()
         {
-            
+            var a = new DoublyLinkedList<int>();
+            a.AddLast(4);
+            a.AddLast(3);
+            a.AddLast(2);
+            a.AddLast(1);
+            var n = a.First;
+            Console.WriteLine(a.Contains(3));
+            foreach (var i in a)
+            {
+                Console.WriteLine(i);
+            }
+             
         }
     }
 }
