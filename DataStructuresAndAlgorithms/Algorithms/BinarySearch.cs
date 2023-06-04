c2a222667e7fc85a8a59a838369201b97591ae67
Abdullo <abdullakhzukhriddinov@gmail.com>
2023-06-04 18:13:39 +0500

    fetch:Write Algorithm BinarySearch

--- DataStructuresAndAlgorithms/Algorithms/BinarySearch.cs
+++ DataStructuresAndAlgorithms/Algorithms/BinarySearch.cs
@@ -1,6 +1,50 @@
-namespace DataStructuresAndAlgorithms.Algorithms;
+using System.Collections;
 
-public class BinarySearch
+namespace DataStructuresAndAlgorithms
 {
-    
+    /// <summary>
+    /// Only works with sorting Enumerable Collection
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class BinarySearch<T> where T : IComparable<T>
+    {
+        private readonly IEnumerable<T?> _collection;
+
+        public BinarySearch(IEnumerable<T?> collection)
+        {
+            _collection = collection;
+        }
+
+        /// <summary>
+        /// Return a index of searching element
+        /// else return default
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public int Search(T value)
+        {
+            var left = 0;
+            var right = _collection.Count() - 1;
+            while (left <= right)
+            {
+                var mid = left + (right - left) / 2;
+                if (_collection.ElementAt(mid)!.CompareTo(value) == 0)
+                {
+                    return mid;
+                }
+
+                if (_collection.ElementAt(mid)!.CompareTo(value) < 0)
+                {
+                    left = mid + 1;
+                }
+
+                else if (_collection.ElementAt(mid)!.CompareTo(value) > 0)
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return default;
+        }
+    }
 }
